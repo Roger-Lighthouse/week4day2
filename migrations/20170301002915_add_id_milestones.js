d6a8982c20b3f76c2799992204c6aea17660abde
Roger-Lighthouse <roger@whiteshark.ca>
2017-03-01 00:38:56 +0000

    latest migration

---
+++ migrations/20170301002915_add_id_milestones.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('milestones', function(t) {
+        t.integer('famous_person_id');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('milestones', function(t) {
+        t.dropColumn('famous_person_id');
+    });
+};
+
+
+
